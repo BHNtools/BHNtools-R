4a217f49090fa5b1981dff9d243c59ed5e264e72
BHnajlaAir-Any <bhndevtools@gmail.com>
2016-07-12 01:14:58 +0100

    MERGING-SUB-BR :: sub-bhntools-r-test.

---
+++ BHNtools-R-testing/munge/loadWsLib.R
@@ -0,0 +1,24 @@
+#//////////////////////////////////////////////////////////////////
+#__author__ = "BEN HASSINE Najla(bhndevtools@gmail.com)"    #///////
+#__version__ = "1.0.0"                                      #///////
+#__copyright__ = "Copyright BHNtools (c) 2016 BHN"          #///////
+#__license__ = "BHN-TOOLS - DEV"                            #///////
+#__created__ = "JULY 2016 - BHN"                            #///////
+#__modified__ = "JULY 2016  - BHN"                          #///////
+#//////////////////////////////////////////////////////////////////
+
+#************** TOPIC *****************
+#     LOADING LIBRARIES
+#************** TOPIC *****************
+
+#_________ installing Packages
+#install.packages("ggplot2", dependencies = TRUE)
+#install.packages("gridExtra", dependencies = TRUE)
+
+#--------- clean older ws obj
+rm(list = ls())
+
+#--------- loading lib
+library("ggplot2")
+library("gridExtra")
+
